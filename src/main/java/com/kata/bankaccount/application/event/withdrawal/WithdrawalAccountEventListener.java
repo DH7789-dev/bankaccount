60557815c2fae914606d111adfa38ab9c544031e
David <darnaud2@myges.fr>
2023-03-09 00:15:07 +0100

    fix controller and postman file

--- src/main/java/com/kata/bankaccount/application/event/withdrawal/WithdrawalAccountEventListener.java
+++ src/main/java/com/kata/bankaccount/application/event/withdrawal/WithdrawalAccountEventListener.java
@@ -1,18 +1,13 @@
 package com.kata.bankaccount.application.event.withdrawal;
 
-import com.kata.bankaccount.application.command.transaction.CreateTransaction;
-import com.kata.bankaccount.domain.enumerate.TransactionType;
-import com.kata.bankaccount.kernel.command.CommandBus;
 import com.kata.bankaccount.kernel.event.EventListener;
 
 import java.util.Date;
 
 public class WithdrawalAccountEventListener implements EventListener<WithdrawalAccountEvent> {
-    private CommandBus commandBus;
+
     @Override
     public void listenTo(WithdrawalAccountEvent event) {
-        CreateTransaction createTransaction = new CreateTransaction(TransactionType.DEPOSIT,new Date().toString());
-        commandBus.send(createTransaction);
-        System.out.println("listening DepositAccountEvent.");
+        System.out.println("listening WithdrawalAccountEvent.");
     }
 }
--- src/main/java/com/kata/bankaccount/web/controller/AccountController.java
+++ src/main/java/com/kata/bankaccount/web/controller/AccountController.java
@@ -3,11 +3,13 @@ package com.kata.bankaccount.web.controller;
 import com.kata.bankaccount.application.command.account.create.CreateAccount;
 import com.kata.bankaccount.application.command.account.deposit.DepositAccount;
 import com.kata.bankaccount.application.command.account.withdrawal.WithdrawalAccount;
+import com.kata.bankaccount.application.command.transaction.CreateTransaction;
 import com.kata.bankaccount.application.query.account.RetrieveBalanceAccount;
 import com.kata.bankaccount.application.query.transaction.RetrieveTransaction;
 import com.kata.bankaccount.domain.entities.Account;
 import com.kata.bankaccount.domain.entities.AccountId;
 import com.kata.bankaccount.domain.entities.Transaction;
+import com.kata.bankaccount.domain.enumerate.TransactionType;
 import com.kata.bankaccount.kernel.command.CommandBus;
 import com.kata.bankaccount.kernel.query.QueryBus;
 import com.kata.bankaccount.web.request.AccountRequest;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,7 +46,7 @@ public class AccountController {
     }
 
 
-    @GetMapping(path = "/transactions", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = "/transactions", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<TransactionsResponse> getAllTransaction() {
         final List<Transaction> Transactions = queryBus.send(new RetrieveTransaction());
         TransactionsResponse TransactionsResponseResult = new TransactionsResponse(Transactions.stream().map(
@@ -51,12 +54,14 @@ public class AccountController {
         return ResponseEntity.ok(TransactionsResponseResult);
     }
 
-    @GetMapping(path = "/actual/{accountId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = "/actual/{accountIdPath}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<AccountResponse> getActualBalance(@PathVariable int accountIdPath) {
         AccountId requestId = AccountId.of(accountIdPath);
         RetrieveBalanceAccount retrieveBalanceAccount = new RetrieveBalanceAccount(requestId);
         Account account = queryBus.send(retrieveBalanceAccount);
         AccountResponse accountResponse = new AccountResponse(account.getId().getValue(),account.getAmount());
+        CreateTransaction createTransaction = new CreateTransaction(TransactionType.CONSULT,new Date().toString());
+        commandBus.send(createTransaction);
          return ResponseEntity.ok(accountResponse);
     }
 
@@ -64,23 +69,27 @@ public class AccountController {
     public ResponseEntity<AccountId> create(@RequestBody @Valid AccountRequest request) {
         CreateAccount createAccount = new CreateAccount(request.amount);
         AccountId accountId = commandBus.send(createAccount);
-        return ResponseEntity.ok(accountId);
+        return new ResponseEntity<AccountId>(accountId, HttpStatus.CREATED);
     }
 
-    @PostMapping(path = "/deposit/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> deposit(@RequestBody @Valid DepositAccountRequest request, @PathVariable int accountIdPath) {
+    @PostMapping(path = "/deposit/{accountIdPath}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccountId> deposit(@RequestBody @Valid DepositAccountRequest request, @PathVariable int accountIdPath) {
         AccountId requestId = AccountId.of(accountIdPath);
         DepositAccount createAccount = new DepositAccount(requestId,request.deposit);
         AccountId accountId = commandBus.send(createAccount);
-        return ResponseEntity.created(URI.create("/account/deposit" + accountId.getValue())).build();
+        CreateTransaction createTransaction = new CreateTransaction(TransactionType.DEPOSIT,new Date().toString());
+        commandBus.send(createTransaction);
+        return new ResponseEntity<AccountId>(accountId, HttpStatus.OK);
     }
 
-    @PostMapping(path = "/withdrawal/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> withdrawal(@RequestBody @Valid WithdrawalAccountRequest request, @PathVariable int accountIdPath) {
+    @PostMapping(path = "/withdrawal/{accountIdPath}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccountId> withdrawal(@RequestBody @Valid WithdrawalAccountRequest request, @PathVariable int accountIdPath) {
         AccountId requestId = AccountId.of(accountIdPath);
         WithdrawalAccount createAccount = new WithdrawalAccount(requestId,request.withdrawal);
         AccountId accountId = commandBus.send(createAccount);
-        return ResponseEntity.created(URI.create("/account/withdrawal" + accountId.getValue())).build();
+        CreateTransaction createTransaction = new CreateTransaction(TransactionType.WITHDRAWAL,new Date().toString());
+        commandBus.send(createTransaction);
+        return new ResponseEntity<AccountId>(accountId, HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
